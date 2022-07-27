167bdf46f655f81a250ede035fa61f0496282506
위지훈 <wijihoon123@naver.com>
2022-07-27 15:39:04 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+		Scanner Scan = new Scanner(System.in);
+		
+		int num1 = Scan.nextInt();
+
+		System.out.println(num1 - 543);
+			
+	}
+
+}
