d07c0fc999f0f8bdf7f45d1626e593faa17a4bf1
위지훈 <wijihoon123@naver.com>
2022-07-27 15:31:38 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+		Scanner Scan = new Scanner(System.in);
+		
+		int num1 = Scan.nextInt();
+		int num2 = Scan.nextInt();
+		
+		System.out.println((double) num1 / num2);
+			
+	}
+
+}
