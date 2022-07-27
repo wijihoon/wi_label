ffe09d93e22ccff764d16524bf49355ba32e1111
위지훈 <wijihoon123@naver.com>
2022-07-27 15:36:49 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+		Scanner Scan = new Scanner(System.in);
+		
+		String str1 = Scan.nextLine();
+
+		System.out.println(str1 + "??!");
+			
+	}
+
+}
