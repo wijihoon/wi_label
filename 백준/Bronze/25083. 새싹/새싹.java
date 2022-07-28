49c1a32e3f556ac76de582bd7aeef52890aea15c
위지훈 <wijihoon123@naver.com>
2022-07-28 15:14:08 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11208 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+				   "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+				   " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+				   "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+				   "      |\n" + 			// \n 이 제어문자다.
+				   "      |";
+ 
+		System.out.print(s);
+	}
+}
