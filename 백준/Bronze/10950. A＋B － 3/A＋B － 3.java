3d61bdb0ad1a6bf004bab7672454fbfdbe332d18
위지훈 <wijihoon123@naver.com>
2022-07-28 18:04:05 +0900

    [Bronze V] Title: A+B - 3, Time: 224 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int num1 = sc.nextInt();
+    	
+    	for (int i = 0; i < num1; i++){
+    	    System.out.println(sc.nextInt() + sc.nextInt());
+    	}
+	}
+
+}
