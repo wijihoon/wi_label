8f687e3bb08e6d004e7a8ef7d10f6fc5d54fb1d4
위지훈 <wijihoon123@naver.com>
2022-07-28 18:01:05 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18576 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int num1 = sc.nextInt();
+    	
+    	for(int i = 1; i < 10; i++){
+    	    System.out.println(num1 + " * " + i + " = " + num1 * i);
+    	}
+	}
+
+}
