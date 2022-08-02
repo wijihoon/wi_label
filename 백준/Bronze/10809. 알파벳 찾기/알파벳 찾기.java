87844ebdb296a9894e55141f9f362be4c5708202
위지훈 <wijihoon123@naver.com>
2022-08-02 15:32:23 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 148 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+	    
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        String[] strArr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        
+        for(String s : strArr){
+        	bw.write(str.indexOf(s) + " ");
+        }
+        
+    	bw.close();
+    }
+}
