daf33a0e9b1572bd3cead897fb1babadd9c121f7
위지훈 <wijihoon123@naver.com>
2022-10-07 17:17:03 +0900

    [unrated] Title: 세균 증식, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int n, int t) {
-        
-        for(int i = 0; i < t; i++){
-            n *= 2;
-        }
-        return n;
+        return n << t;
     }
 }
