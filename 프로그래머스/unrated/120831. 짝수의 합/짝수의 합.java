0c7ec5b833059510df40338d599ce1a14a7b426e
위지훈 <wijihoon123@naver.com>
2022-10-07 09:58:16 +0900

    [unrated] Title: 짝수의 합, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 2; i < n + 1; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
