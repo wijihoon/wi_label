ba0be4db963d93cafbab83c0e87a50088a3db4ae
위지훈 <wijihoon123@naver.com>
2022-10-11 22:16:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int N;
+    static long sum, answer;
+    
+    public long solution(int n) {
+        long[] longArr = new long[2001];
+        longArr[1] = 1;
+        longArr[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            longArr[i] = (longArr[i - 1] + longArr[i - 2]) % 1234567;
+        }
+
+        return longArr[n];
+    }
+}
