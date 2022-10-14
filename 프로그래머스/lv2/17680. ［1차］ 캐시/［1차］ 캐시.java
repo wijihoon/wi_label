903523cec6ba6fcef961bab7bf32f56365e599ab
위지훈 <wijihoon123@naver.com>
2022-10-14 10:00:00 +0900

    [level 2] Title: [1차] 캐시, Time: 1.86 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-        ArrayList<String> cache = new ArrayList<>();
+        if(cacheSize == 0) return cities.length * 5;
+	    
+	    int answer = 0;
         
-        if(cacheSize == 0) // 캐시크기가 0
-            return cities.length * 5;
-        
-        for(int i=0; i<cities.length; i++) {
-            cities[i] = cities[i].toUpperCase(); // 대소문자 구분X
-            if(cache.contains(cities[i])) { // cache hit
-                cache.remove(cities[i]);
-                cache.add(cities[i]);
-                answer += 1;
-            }
-            else { // cache miss
-                if(cache.size()==cacheSize) {
-                    cache.remove(0);
-                    cache.add(cities[i]);
-                }
-                else
-                    cache.add(cities[i]);
-                answer += 5;
-            }
-        }
-        return answer;
+	    ArrayList<String> arrList = new ArrayList<>();
+	    
+	    for(int i = 0; i < cities.length; i++){
+	        cities[i] = cities[i].toLowerCase();
+	        
+	        if(arrList.contains(cities[i])){
+	            answer++;
+	            arrList.remove(cities[i]);
+                arrList.add(cities[i]);
+	        }else{
+	            if(arrList.size() == cacheSize){
+    	            arrList.remove(0);
+    	            arrList.add(cities[i]);
+	                
+	            }else{
+	                arrList.add(cities[i]);
+	            }
+	            answer += 5;
+	        }
+	    }
+	    
+	    return answer;
     }
 }
